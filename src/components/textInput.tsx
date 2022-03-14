eef13b755264b2112c7ad8ec119b771ad04c2cf2
Sadia Tasnim <s.sharna06@gmail.com>
2022-03-15 00:59:25 +0600

    .

--- src/components/textInput.tsx
+++ src/components/textInput.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 
 export function TextInput({ icon, ...rest }: any) {
-  console.log(icon)
   return <div className="input-group mb-3">
     <input {...rest} />
     {icon ? <span className="input-group-text material-icons-outlined" id="basic-addon2">{icon} </span>
